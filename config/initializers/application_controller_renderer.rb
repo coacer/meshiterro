2dccf3873e69d99c83e8a5413c8b4c900ba484eb
coacer <coacer728h23@icloud.com>
2019-04-07 15:24:57 +0900

    Finish post_image functions

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f68cb1d9ff6da404a29a7285e8274f19e45f99b084e1cd117c67f0f0de7b793fc7acb3a36bfe24302316a08b06471ba2c212d9ac9fa015ccba96c04e62529cc6'
